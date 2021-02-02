e00df1688e9c591ff41991b7b2ec39a16e7b76f1
Pradeep LN <pradeep.ln78@gmail.com>
2021-02-02 11:16:42 +0530

    initial commit

---
+++ src/com/rakuten/training/basics/Stack.java
@@ -0,0 +1,8 @@
+package com.rakuten.training.basics;
+
+public interface Stack {
+//	public static final Object[] contents = new Object[100]; 
+	public void push(Object anElement);
+	public Object pop();
+
+}
