dbdf56ed071d2e5771516f25d5f1045f65a6e75f
pradeepln <41988925+pradeepln@users.noreply.github.com>
2021-02-02 11:48:33 +0530

    added comment from github

--- src/com/rakuten/training/basics/Stack.java
+++ src/com/rakuten/training/basics/Stack.java
@@ -1,5 +1,5 @@
 package com.rakuten.training.basics;
-
+//this is comment from github editor
 public interface Stack {
 //	public static final Object[] contents = new Object[100]; 
 	public void push(Object anElement);
